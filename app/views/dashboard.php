6ba0645f257c94d90c0344c50fd881aaf870ece3
RAKOTOMALALA Sanorana Coralie Christnah <coralie.rakotomalala@gmail.com>
2026-02-16 16:30:20 +0300

    mettre un lien de besoins.php -> Insertion

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -110,7 +110,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  " href="<?= BASE_URL ?>/virtual-reality">
+          <a class="nav-link  " href="<?= BASE_URL ?>/form">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>box-3d-50</title>
@@ -127,7 +127,7 @@
                 </g>
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Virtual Reality</span>
+            <span class="nav-link-text ms-1">Insertion</span>
           </a>
         </li>
         <li class="nav-item">
